552da4458dfd49c49eb692d4a4947759ea6024f2
Ashkan <ashkan.dant3@gmail.com>
2015-02-15 17:45:07 +0330

    Hello, World!

--- intent/intent/urls.py
+++ intent/intent/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'intent.views.home', name='home'),
+    url(r'^$', 'intentweb.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
